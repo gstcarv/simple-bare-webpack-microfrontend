b93b7fed3c9031012fc2fd699d899346dd7bb2d0
Gustavo Carvalho <gstcarv@gmail.com>
2022-02-23 18:23:50 -0300

    created mount functions on the remotes to correctly render elements inside or outside project isolation

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,7 @@
-import 'products/ProductsIndex'
-import 'cart/CartShow'
+import { mount as mountProducts } from 'products/ProductsIndex'
+import { mount as mountCart } from 'cart/CartShow'
 
-console.log('Container!')
+console.log('Container!');
+
+mountProducts(document.querySelector("#dev-products__host"));
+mountCart(document.querySelector("#dev-cart__host"));
